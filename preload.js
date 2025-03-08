74257abc1e6f60cc7f5e73894713c5c0f0e351f9
soroush <soroushdeveloper20@gmail.com>
2025-03-08 17:07:26 +0330

    deleted electron

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-    setdns: (cmd) => ipcRenderer.send('setdns', cmd),
-    onCommandOutput: (callback) => ipcRenderer.on('command-output', (event, data) => callback(data)),
-    getos: () => ipcRenderer.invoke('getos')
-});
